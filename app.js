03d3285c4342e29802b4a04f2c0c8254bcb2ad5b
Abrahan Albarran <ajau88@hotmail.com>
2025-08-05 15:58:39 -0400

    Se agrega función agregar amigo

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("Ejecutando el challenge")
 
 let listaDeAmigos = [];
 const LIMITE_AMIGOS = 10;
+
+//Función de agregar amigo
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    input.value.trim();
+    
+}
