44d7c9f36af7be2d1000a62f333cf213b4c20488
Abrahan Albarran <ajau88@hotmail.com>
2025-08-05 20:11:32 -0400

    Se agrego validación para nombres

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const LIMITE_AMIGOS = 10;
 
 //Función de agregar amigo
 function agregarAmigo() {
-    const input = document.getElementById('amigo');
-    input.value.trim();
+    const nombre = estado.elmentos.input.value.trim();
 
 }
