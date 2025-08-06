5364091de9f843d749f4fa9ac3952773a450ffa1
Abrahan Albarran <ajau88@hotmail.com>
2025-08-05 20:21:07 -0400

    se agrego lista mas actualización de interfaz y mensajes de confirmación

--- app.js
+++ app.js
@@ -5,4 +5,24 @@ const LIMITE_AMIGOS = 10;
 function agregarAmigo() {
     const nombre = estado.elmentos.input.value.trim();
 
+    //Validación de nombres
+    const validacion = validarNombre(nombre);
+    if (!validacion.valido) {
+        mostrarMensaje(validacion.mensaje, 'error');
+        estado.elmentos.input.focus();
+        return;
+    }
+
+    //Agregar a la lista
+    estado.listaDeAmigos.push(nombre);
+    estado.elmentos.imput.value ='';
+
+    //Actualiza interfaz
+    mostarLista();
+    actualizarDisponible();
+
+    //Mostar mensaje de confirmación
+    mostrarMensaje(`${nombre} Agregado correctamente`, 'success');
+    //Mantener focus para contuar agregando
+    estado.elmentos,input.focus();
 }
